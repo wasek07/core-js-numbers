daf40cad8e1c8cd53237058c7eeaa2c2990b0729
wasek07 <wasek07@ya.ru>
2023-12-21 23:16:14 +0100

    feat: add solution for getting last digit

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -125,8 +125,9 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  const lastDigit = value % 10;
+  return lastDigit;
 }
 
 /**
